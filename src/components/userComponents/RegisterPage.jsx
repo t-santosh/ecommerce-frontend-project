9aef52664efd6615c8c7fa6d6b2c204f518c45d5
GTS-Intern-Santosh <gtsintern1@gmail.com>
2025-02-27 01:21:15 +1100

    Changes made to the file names and added some css

--- src/components/userComponents/RegisterPage.jsx
+++ src/components/userComponents/RegisterPage.jsx
@@ -1,9 +1,80 @@
 import React from 'react';
+import '../../assets/styles/registerPage.css';
 
 const RegisterPage = () => {
   return (
     <div>
-      <h1>Welcome to Register page</h1>
+      <div className='container-fluid bg-light'>
+        <a
+          id='signin-btn'
+          class='btn btn-outline-primary btn-sm'
+          href='#'
+          role='button'>
+          Sign In
+        </a>
+        <div className='title'>
+          <h1 id='brand-name'>
+            <a href='#'>E-Commerce</a>
+          </h1>
+          <div className='sub-title'>One shopping stop</div>
+        </div>
+        <span>Don't have an account?</span>
+        <div className='card text-bg-light shadow'>
+          <h3 className='text-center mb-4 mt-3'>Sign up</h3>
+          <br />
+          <form>
+            <div className='mb-3'>
+              <label htmlFor='first-name' className='form-label'>
+                First name
+              </label>
+              <input
+                type='text'
+                className='form-control shadow'
+                id='name'
+                placeholder='Enter your first name'
+              />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='Last-name' className='form-label'>
+                Last name
+              </label>
+              <input
+                type='text'
+                className='form-control shadow'
+                id='name'
+                placeholder='Enter your last name'
+              />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='email' className='form-label'>
+                Email address
+              </label>
+              <input
+                type='email'
+                className='form-control shadow'
+                id='email'
+                placeholder='example@example.com'
+              />
+            </div>
+            <div className='mb-3'>
+              <label htmlFor='password' className='form-label'>
+                Password
+              </label>
+              <input
+                type='password'
+                className='form-control shadow'
+                id='password'
+                placeholder='Enter your password'
+              />
+            </div>
+            <button
+              type='submit'
+              className='btn btn-outline-primary w-100 mt-4 shadow-lg'>
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from '../components/userComponents/LoginPage';
 import RegisterPage from '../components/userComponents/RegisterPage';
-import LandingPage from '../components/LandingPage';
+import HomePage from '../components/userComponents/HomePage';
 
 const UserRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' Component={LandingPage} />
-        <Route path='/register' Component={RegisterPage} />
+        <Route path='/' Component={RegisterPage} />
+        <Route path='/home' Component={HomePage} />
         <Route path='/login' Component={LoginPage} />
       </Routes>
     </BrowserRouter>
