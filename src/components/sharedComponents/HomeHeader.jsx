578d8e888695ff39c5af0fb6f782441d5f009be1
GTS-Intern-Santosh <gtsintern1@gmail.com>
2025-03-07 23:55:43 +1100

    Conditional UI rendering implemented

--- src/components/sharedComponents/HomeHeader.jsx
+++ src/components/sharedComponents/HomeHeader.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import global from '../../constants/global';
 import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const HomeHeader = () => {
   const navigate = useNavigate(); // Get navigation function
+  const user = useSelector((state) => state.user.user);
+  const isAdmin = user?.isAdmin;
 
   const handleLogout = () => {
     localStorage.removeItem('token'); // Remove token from local storage
@@ -18,38 +21,43 @@ const HomeHeader = () => {
           </Link>
           <div className='collapse navbar-collapse'>
             <ul className='navbar-nav me-auto'>
-              <li className='nav-item'>
-                <Link className='nav-link' to={'/dashboard'}>
-                  Dashboard
-                </Link>
-              </li>
+              {/* Render the Dashboard link only for admins */}
+              {isAdmin && (
+                <li className='nav-item'>
+                  <Link className='nav-link' to={'/dashboard'}>
+                    Dashboard
+                  </Link>
+                </li>
+              )}
               <li className='nav-item'>
                 <Link className='nav-link' to={'/home'}>
                   Home
                 </Link>
               </li>
-              <li className='nav-item dropdown'>
-                <Link
-                  className='nav-link'
-                  to='#'
-                  role='button'
-                  data-bs-toggle='dropdown'
-                  aria-expanded='false'>
-                  Products
-                </Link>
-                <ul className='dropdown-menu border rounded shadow'>
-                  <li>
-                    <Link className='dropdown-item' to={'/create-product'}>
-                      Insert Product
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className='dropdown-item' to={'/view-products'}>
-                      View Products
-                    </Link>
-                  </li>
-                </ul>
-              </li>
+              {isAdmin && (
+                <li className='nav-item dropdown'>
+                  <Link
+                    className='nav-link'
+                    to='#'
+                    role='button'
+                    data-bs-toggle='dropdown'
+                    aria-expanded='false'>
+                    Products
+                  </Link>
+                  <ul className='dropdown-menu border rounded shadow'>
+                    <li>
+                      <Link className='dropdown-item' to={'/create-product'}>
+                        Insert Product
+                      </Link>
+                    </li>
+                    <li>
+                      <Link className='dropdown-item' to={'/view-products'}>
+                        View Products
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+              )}
               <li className='nav-item'>
                 <Link className='nav-link' to={'/about'}>
                   About Us
@@ -67,27 +75,41 @@ const HomeHeader = () => {
                 Search
               </button>
             </form>
-            <div className='navbar-nav dropdown'>
-              <Link to='/profile'>
+            <li className='navbar-nav nav-item me-1'>
+              <Link className='nav-link' to={'/cart'}>
+                Cart
+              </Link>
+            </li>
+            <li className='navbar-nav nav-item dropdown'>
+              <Link
+                className='nav-link'
+                role='button'
+                data-bs-toggle='dropdown'
+                aria-expanded='false'>
                 <img
                   src='/images/arush.jpeg'
                   alt='Profile'
                   className='rounded-circle'
                   width='30'
                   height='30'
-                  style={{ cursor: 'pointer' }}
                 />
               </Link>
-            </div>
-            <div className='navbar-nav me-4'>
-              <li className='nav-item'>
-                <button
-                  className='nav-link btn btn-link'
+              <ul
+                className='dropdown-menu border rounded shadow'
+                style={{ right: '0', left: 'auto' }}>
+                <li>
+                  <Link className='dropdown-item' to={'/profile'}>
+                    Profile
+                  </Link>
+                </li>
+                <li
+                  className='dropdown-item'
+                  role='button'
                   onClick={handleLogout}>
                   Logout
-                </button>
-              </li>
-            </div>
+                </li>
+              </ul>
+            </li>
           </div>
         </div>
       </nav>
